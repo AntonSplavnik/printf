f380d1391260ab0a7b1765dbd747b0f7201c9bae
Anton Splavnik <antonsplavnik@gmail.com>
2024-01-08 19:37:50 +0100

    fisrt commit

--- Makefile
+++ Makefile
@@ -2,18 +2,22 @@ CC = gcc
 CFLAGS = -Wall -Wextra -Werror
 NAME = libftprintf.a
 SRCS =
+LIBFT = libft
 OBJS = $(SRCS:.c=.o)
 
 all: $(NAME)
 
 $(NAME): $(OBJS)
-	ar rc $(NAME) $(OBJS) -c $< -o $@
+	make -C $(LIBFT)
+	ar rc $(NAME) $(OBJS) -L./$(LIBFT) -lft -c $< -o $@
 
 clean:
 	rm -rf $(OBJS)
+	rm -rf $(LIBFT)/*.o
 
 fclean: clean
 	rm -Rf $(NAME)
+	rm -Rf $(LIBFT)/libft.a
 
 re: fclean all
 
--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,41 @@
 /*   By: asplavni <asplavni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 14:54:24 by asplavni          #+#    #+#             */
-/*   Updated: 2023/12/21 15:09:39 by asplavni         ###   ########.fr       */
+/*   Updated: 2024/01/08 19:36:14 by asplavni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *format, ...)
-{
+#include "libft/libft.h"
 
+int	ft_printfripoff(const char *format, ...)
+{
+	int	i = 0;
+	int	num_args = ft_strlen(format);
+	va_list args;
+
+	va_start(args, format);
+	while (i < num_args)
+	{
+		if (format[i] == "%")
+			i++;
+			if(format[i] == "%")
+				ft_putchar_fd(v, 1);
+			if(format[i] == "c")
+				ft_putchar_fd(format[i], 1);
+			if(format[i] == "s")
+				ft_putstr_fd(format, 1);
+			if(format[i] == "p")
+			if(format[i] == "d" || format[i] == "i")
+			if(format[i] == "u")
+			if(format[i] == "x")
+			if(format[i] == "X")
+			if(format[i] == "X")
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_printfripoff("%d %f %d", 4, 2, 4);
+	return (0);
 }
